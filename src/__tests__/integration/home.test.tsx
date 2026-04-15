20d6f8145a6530f445334895ca750f15b63b3221
Arun kumar <61176789+nakkumar@users.noreply.github.com>
2026-04-15 06:22:47 +0530

    Update home.test.tsx

--- src/__tests__/integration/home.test.tsx
+++ src/__tests__/integration/home.test.tsx
@@ -1,5 +1,5 @@
 import HomePage from '@/pages/home-page';
-#import { render, screen } from '@testing-library/react';
+//import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { BrowserRouter } from 'react-router-dom';
 import axios from 'axios';
